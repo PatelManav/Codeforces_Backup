1a10b3cf64dae2916ba13afebdde84b6eaf998a4
helewrer3 <helewrer3@gmail.com>
2020-07-26 19:04:48 +0530

    26 July, accidentally deleted code

---
+++ temp.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define ll long long
+#define vll vector<long long>
+#define mll map<long long, long long>
+#define f first
+#define s second
+#define pb emplace_back
+#define all(a) a.begin(),a.end()
+#define rall(a) a.end(),a.begin()
+using namespace std;
+
+void solve(){
+	
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.precision(20);
+	ll t;
+	t = 1;
+	//cin >> t;
+	while(t--)solve();
+	cout << endl;
+	return 0;
+}
